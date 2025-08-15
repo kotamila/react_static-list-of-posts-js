96a09f5723edc08140dd9cf876394ee4eb4db577
Tamila Kochereshchenko <tamila272005@icloud.com>
2025-08-15 14:14:06 +0300

    try to do all

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,17 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+
+export const PostInfo = ({ info }) => {
+  if (!info) {
+    return null;
+  }
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{info.title}</h3>
+        <p>{'Posted by '}</p>
+      </div>
+      <p className="PostInfo__body">{info.body}</p>
+    </div>
+  );
+};
