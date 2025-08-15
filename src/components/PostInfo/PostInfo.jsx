86e23743eb16183c52088bc183242a0472de5bcb
Tamila Kochereshchenko <tamila272005@icloud.com>
2025-08-15 15:10:14 +0300

    try to fix tests 2

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,5 +1,6 @@
 import './PostInfo.scss';
 import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
 
 export const PostInfo = ({ post }) => {
   if (!post) {
@@ -10,10 +11,15 @@ export const PostInfo = ({ post }) => {
     <div className="PostInfo">
       <div className="PostInfo__header">
         <h3 className="PostInfo__title">{post.title}</h3>
-        <UserInfo user={post.user} />
         <p>{'Posted by '}</p>
+        <UserInfo user={post.user} />
       </div>
       <p className="PostInfo__body">{post.body}</p>
+      {post.comments.length !== 0 ? (
+        <CommentList comments={post.comments} />
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
     </div>
   );
 };
