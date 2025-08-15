cc49477164177c3a685735e7893b312cf8cf1e7d
Tamila Kochereshchenko <tamila272005@icloud.com>
2025-08-15 14:47:48 +0300

    try to fix tests

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,17 +1,19 @@
 import './PostInfo.scss';
+import { UserInfo } from '../UserInfo';
 
-export const PostInfo = ({ info }) => {
-  if (!info) {
+export const PostInfo = ({ post }) => {
+  if (!post) {
     return null;
   }
 
   return (
     <div className="PostInfo">
       <div className="PostInfo__header">
-        <h3 className="PostInfo__title">{info.title}</h3>
+        <h3 className="PostInfo__title">{post.title}</h3>
+        <UserInfo user={post.user} />
         <p>{'Posted by '}</p>
       </div>
-      <p className="PostInfo__body">{info.body}</p>
+      <p className="PostInfo__body">{post.body}</p>
     </div>
   );
 };
